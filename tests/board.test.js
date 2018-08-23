84e56cf07b08265f21ea45121e588f0c31024357
Matthew Keller <matthewkeller@Matthews-MacBook-Pro.local>
2018-08-23 11:56:55 +0400

    renamed the Class board to class WordSearch

--- tests/board.test.js
+++ tests/board.test.js
@@ -1,44 +1,47 @@
-const Board = require('../app/board');
+const WordSearch = require('../app/board');
 
 
 describe("board should have an array of words to search for", () => {
-    let board;
+    let wordSearch;
 
     beforeEach(() => {
-        board = new Board()
+        wordSearch = new WordSearch()
     });
 
     test("words to search for should be an array", () => {
-        expect(Array.isArray(board.wordsToSearchFor)).toBe(true)
+        expect(Array.isArray(wordSearch.wordsToSearchFor)).toBe(true)
     });
 
     test("words to search array elements should be strings", () => {
-        expect(board.wordsToSearchFor.every(function(item){ return typeof item === "string" })).toBe(true)
+        expect(wordSearch.wordsToSearchFor.every(function(item){ return typeof item === "string" })).toBe(true)
     })
     
 })
 
+describe("board should have an array of letters that represent the board", () => {
+   
+})
 
 
 describe('Board setup building an array that represents a square board', () => {
-    let board;
+    let wordSearch;
 
     beforeEach(() => {
-        board = new Board()
+        wordSearch = new WordSearch()
     });
 
     test('Set up should produce an array', () => {
-        expect(Array.isArray(board.setUp())).toBe(true)
+        expect(Array.isArray(wordSearch.setUp())).toBe(true)
     });
 
     test('Set up should produce an array of arrays', () => {
-        expect(board.setUp().every(function(row){ 
+        expect(wordSearch.setUp().every(function(row){ 
             return Array.isArray(row)
         })).toBe(true)
     });
 
     test("Set up array's length should be equal to the length of the sub arrays", () => {
-        expect(board.setUp().length).toEqual(board.setUp()[0].length)
+        expect(wordSearch.setUp().length).toEqual(wordSearch.setUp()[0].length)
     });
 
     
