fb9d140da5a457739c56dfbdb297242fea0a84ee
Matthew Keller <matthewkeller@Matthews-MacBook-Pro.local>
2018-08-23 17:37:57 +0400

    merging build board branch to master

--- app/solveWordSearch.js
+++ app/solveWordSearch.js
@@ -2,12 +2,27 @@
 const WordSearchConfiguration = require('./boardSetUp');
 
 class SolveWordSearch {
-    constructor () {
-        this.wordSearchConfiguration = new WordSearchConfiguration()
-        this.wordSearchConfiguration.setUp()
-        this.board = this.wordSearchConfiguration.board
-        this.wordsToSearchFor = this.wordSearchConfiguration.wordsToSearchFor
+    constructor (words) {
+        console.log(words)
+    }
+
+    searchHorizontally (word, board) {
+        for (let i = 0; i < word.length; i++) {
+            board.forEach((row, rowIndex) => {
+                row.forEach((letter, letterIndex) => {
+                    if(word[0] === letter) {
+                        rowIndex, letterIndex
+                    }
+                })
+            })
+        }
+
     }
 }
+
+// const solveWordSearch = new SolveWordSearch()
+// solveWordSearch.searchHorizontally('ARPADELO', solveWordSearch.board)
+
+
 
 module.exports = SolveWordSearch;
--- tests/solveWordSearch.test.js
+++ tests/solveWordSearch.test.js
@@ -1,23 +1,32 @@
 const SolveWordSearch = require('../app/solveWordSearch');
 const WordSearchConfiguration = require('../app/boardSetUp');
 
-test("imported word search configuration to class SolveWordSearch", () => {
-    const solveWordSearch = new SolveWordSearch()
+// test("word search configuration should be imported to class SolveWordSearch", () => {
+//     const solveWordSearch = new SolveWordSearch()
 
-    const wordSearchConfiguration = new WordSearchConfiguration()
+//     const wordSearchConfiguration = new WordSearchConfiguration()
 
-    wordSearchConfiguration.setUp()
+//     wordSearchConfiguration.setUp()
 
-    expect(solveWordSearch.wordSearchConfiguration.board).toEqual(wordSearchConfiguration.board)
+//     expect(solveWordSearch.wordSearchConfiguration.board).toEqual(wordSearchConfiguration.board)
 
-    expect(solveWordSearch.wordSearchConfiguration.wordsToSearchFor).toEqual(wordSearchConfiguration.wordsToSearchFor)
-})
+//     expect(solveWordSearch.wordSearchConfiguration.wordsToSearchFor).toEqual(wordSearchConfiguration.wordsToSearchFor)
+// })
 
 describe("solution should find words to find on the board horizontally", () => {
     let solveWordSearch;
 
+    let wordSearchConfiguration;
+
     beforeEach(() => {
-        solveWordSearch = new SolveWordSearch()
+        wordSearchConfiguration = new WordSearchConfiguration()
+        wordSearchConfiguration.setUp()
+        solveWordSearch = new SolveWordSearch(wordSearchConfiguration.wordsToSearchFor)
+        
     });
+
+    test("", () => {
+
+    })
 
 })
