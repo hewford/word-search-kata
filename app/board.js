1873c36300f3a35db8d9259fd2c5640376dc370c
Matthew Keller <matthewkeller@Matthews-MacBook-Pro.local>
2018-08-23 11:08:55 +0400

    board.setUp returns an array with a sub array

--- app/board.js
+++ app/board.js
@@ -8,7 +8,7 @@ class Board {
 
   setUp() {
 
-    return []
+    return [[]]
   }
     
   
--- tests/board.test.js
+++ tests/board.test.js
@@ -2,9 +2,18 @@ const Board = require('../app/board');
 
 
 describe('Board setup building an array that represents a square board', () => {
+    let board;
+
+    beforeEach(() => {
+        board = new Board()
+    });
+
     test('Set up should produce an array', () => {
-        const board = new Board();
-        console.log(board.setUp())
         expect(Array.isArray(board.setUp())).toBe(true)
-    })
+    });
+
+    test('Set up should produce an array of arrays', () => {
+        expect(Array.isArray(board.setUp()[0])).toBe(true)
+    });
+
 })
