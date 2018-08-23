773e3deed4a004d7b1e16c5ad2a57d6c441fe3cf
Matthew Keller <matthewkeller@Matthews-MacBook-Pro.local>
2018-08-23 11:15:49 +0400

    board set up array length is equal to sub array length

--- tests/board.test.js
+++ tests/board.test.js
@@ -1,6 +1,5 @@
 const Board = require('../app/board');
 
-
 describe('Board setup building an array that represents a square board', () => {
     let board;
 
@@ -16,4 +15,7 @@ describe('Board setup building an array that represents a square board', () => {
         expect(Array.isArray(board.setUp()[0])).toBe(true)
     });
 
+    test("Set up array's length should be equal to the length of the sub arrays", () => {
+        expect(board.setUp().length).toEqual(board.setUp()[0].length)
+    })
 })
