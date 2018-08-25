4cefe45ab306023d2ec03d0c103fbc3b34f4e243
Matthew Keller <matthewkeller@Matthews-MacBook-Pro.local>
2018-08-25 16:26:33 +0400

    re-organized test spacing so that the board inputs are easier to see and read

--- tests/solveWordSearch.test.js
+++ tests/solveWordSearch.test.js
@@ -45,7 +45,10 @@ describe("solution should find words to find on the board horizontally", () => {
 
     test("if a word does not exist, should return 'word not found'", () => {
         expect(
-            wordSearchQuery.startSearchQuery('AZ', [['B', 'B', 'B'], ['B', 'B', 'A'], ['B', 'B', 'B']])
+            wordSearchQuery.startSearchQuery('AZ', [
+                ['B', 'B', 'B'], 
+                ['B', 'B', 'A'], 
+                ['B', 'B', 'B']])
         ).toBe("word not found")
     })
 
@@ -62,19 +65,28 @@ describe("solution should find words to find on the board horizontally", () => {
 
     test("horizontal search can find the first letter of a word", () => {
        expect(
-           wordSearchQuery.startSearchQuery('A', [['B', 'B', 'B'], ['C', 'C', 'A'], ['D', 'D', 'D']])
+           wordSearchQuery.startSearchQuery('A', [
+               ['B', 'B', 'B'], 
+               ['C', 'C', 'A'], 
+               ['D', 'D', 'D']])
        ).toBe('(1, 2)')
     })
 
     test("horizontal search moving RIGHT can find the first and second letter of a word", () => {
         expect(
-            wordSearchQuery.startSearchQuery('AZ', [['B', 'B', 'B'], ['C', 'A', 'Z'], ['D', 'D', 'D']])
+            wordSearchQuery.startSearchQuery('AZ', [
+                ['B', 'B', 'B'], 
+                ['C', 'A', 'Z'], 
+                ['D', 'D', 'D']])
         ).toBe('(1, 1), (1, 2)')
      });
 
      test("horizontal search moving RIGHT should find first, second, and third letters and store the correct coordinates even if it comes across the first letter multiple times", () => {
         expect(
-            wordSearchQuery.startSearchQuery('AZB', [['A', 'B', 'B', 'A'], ['A', 'Z', 'B', 'A'], ['D', 'A', 'D', 'D']])
+            wordSearchQuery.startSearchQuery('AZB', [
+                ['A', 'B', 'B', 'A'], 
+                ['A', 'Z', 'B', 'A'], 
+                ['D', 'A', 'D', 'D']])
         ).toBe('(1, 0), (1, 1), (1, 2)')
      });
 
@@ -91,13 +103,19 @@ describe("solution should find words to find on the board horizontally", () => {
 
      test("horizontal search moving LEFT can find the first and second letter of a word", () => {
         expect(
-            wordSearchQuery.startSearchQuery('AC', [['B', 'B', 'B'], ['C', 'A', 'Z'], ['D', 'D', 'D']])
+            wordSearchQuery.startSearchQuery('AC', [
+                ['B', 'B', 'B'], 
+                ['C', 'A', 'Z'], 
+                ['D', 'D', 'D']])
         ).toBe('(1, 1), (1, 0)')
      });
 
      test("horizontal search moving LEFT should find first, second, and third letters and store the correct coordinates even if it comes across the first letter multiple times", () => {
         expect(
-            wordSearchQuery.startSearchQuery('AAC', [['C', 'B', 'C', 'A'], ['A', 'C', 'A', 'A'], ['D', 'D', 'D', 'D']])
+            wordSearchQuery.startSearchQuery('AAC', [
+                ['C', 'B', 'C', 'A'], 
+                ['A', 'C', 'A', 'A'], 
+                ['D', 'D', 'D', 'D']])
         ).toBe('(1, 3), (1, 2), (1, 1)')
      });
 
@@ -121,7 +139,9 @@ describe("solution should find words to find on the board vertically", () => {
 
         wordSearchQuery.foundWord = '(1, 0)'
 
-        wordSearchQuery.searchVerticallyUpwards('AZ', [['Z','D'],['A','D']],  1, 0)
+        wordSearchQuery.searchVerticallyUpwards('AZ', [
+            ['Z','D'],
+            ['A','D']],  1, 0)
 
         expect(
             wordSearchQuery.foundWord
@@ -130,20 +150,29 @@ describe("solution should find words to find on the board vertically", () => {
 
     test("when searching vertically UPWARDS, array should never check for a sub array of a negative index, as undefined is not an array", () => {
         expect(
-            wordSearchQuery.startSearchQuery('AZ', [['A', 'B', 'B'], ['C', 'D', 'A'], ['D', 'D', 'A']])
+            wordSearchQuery.startSearchQuery('AZ', [
+                ['A', 'B', 'B'], 
+                ['C', 'D', 'A'], 
+                ['D', 'D', 'A']])
         ).toBe("word not found")
     })
 
     test("vertical search moving UPWARDS should find first, second, and third letters and store the correct coordinates even if it comes across the first letter multiple times", () => {
         expect(
-            wordSearchQuery.startSearchQuery('AAC', [['A', 'D', 'D', 'C'], ['D', 'D', 'D', 'A'], ['D', 'D', 'D', 'A'], ['D', 'D', 'D', 'A']])
+            wordSearchQuery.startSearchQuery('AAC', [
+                ['A', 'D', 'D', 'C'], 
+                ['D', 'D', 'D', 'A'], 
+                ['D', 'D', 'D', 'A'], 
+                ['D', 'D', 'D', 'A']])
         ).toBe('(2, 3), (1, 3), (0, 3)')
     });
 
     test("vertical search moving DOWNARDS can find the first and second letter of a word when downward search is called directly", () => {
         wordSearchQuery.foundWord = '(0, 0)'
 
-        wordSearchQuery.searchVerticallyDownwards('AZ', [['A','D'],['Z','D']],  0, 0)
+        wordSearchQuery.searchVerticallyDownwards('AZ', [
+            ['A','D'],
+            ['Z','D']],  0, 0)
 
         expect(
             wordSearchQuery.foundWord
@@ -152,19 +181,28 @@ describe("solution should find words to find on the board vertically", () => {
 
     test("vertical search moving DOWNARDS can find the first and second letter of a word", () => {
         expect(
-            wordSearchQuery.startSearchQuery('AZ', [['A', 'D', 'D', 'C'], ['Z', 'D', 'D', 'D']])
+            wordSearchQuery.startSearchQuery('AZ', [
+                ['A', 'D', 'D', 'C'], 
+                ['Z', 'D', 'D', 'D']])
         ).toBe('(0, 0), (1, 0)')
     });
 
     test("when searching vertically DOWNWARDS, array should never check for a sub array of an index that is undefined, as undefined is not an array", () => {
         expect(
-            wordSearchQuery.startSearchQuery('AZ', [['B', 'B', 'B'], ['C', 'D', 'B'], ['D', 'D', 'A']])
+            wordSearchQuery.startSearchQuery('AZ', [
+                ['B', 'B', 'B'], 
+                ['C', 'D', 'B'], 
+                ['D', 'D', 'A']])
         ).toBe("word not found")
     });
 
     test("vertical search moving DOWNWARDS should find first, second, and third letters and store the correct coordinates even if it comes across the first letter multiple times", () => {
         expect(
-            wordSearchQuery.startSearchQuery('AAC', [['A', 'D', 'D', 'D'], ['A', 'D', 'D', 'A'], ['A', 'D', 'D', 'A'], ['C', 'D', 'D', 'A']])
+            wordSearchQuery.startSearchQuery('AAC', [
+                ['A', 'D', 'D', 'D'], 
+                ['A', 'D', 'D', 'A'], 
+                ['A', 'D', 'D', 'A'], 
+                ['C', 'D', 'D', 'A']])
         ).toBe('(1, 0), (2, 0), (3, 0)')
      });
 
@@ -186,7 +224,9 @@ describe("solution should find words to find on the board DIAGONALLU DOWN AND RI
     test("diagonal DOWN RIGHT search should find the first and second letter of a word when downward search is called directly", () => {
         wordSearchQuery.foundWord = '(0, 0)'
 
-        wordSearchQuery.searchDiagonallyDownAndRight('AZ', [['A','D'],['D','Z']],  0, 0)
+        wordSearchQuery.searchDiagonallyDownAndRight('AZ', [
+            ['A','D'],
+            ['D','Z']],  0, 0)
 
         expect(
             wordSearchQuery.foundWord
@@ -196,7 +236,9 @@ describe("solution should find words to find on the board DIAGONALLU DOWN AND RI
     test("diagonal DOWN RIGHT search should not check for a letter in a row that does not exist", () => {
         wordSearchQuery.foundWord = 'testing non-existant rows'
 
-        wordSearchQuery.searchDiagonallyDownAndRight('AZ', [['D','D'],['A','Z']],  1, 0)
+        wordSearchQuery.searchDiagonallyDownAndRight('AZ', [
+            ['D','D'],
+            ['A','Z']],  1, 0)
 
         expect(
             wordSearchQuery.foundWord
@@ -206,14 +248,20 @@ describe("solution should find words to find on the board DIAGONALLU DOWN AND RI
     test("diagonal DOWN RIGHT search should find the first and second letter of a word when startSearchQuery is called", () => {
         
         expect(
-            wordSearchQuery.startSearchQuery('AZ', [['A', 'D', 'D', 'D'], ['D', 'Z', 'D', 'D']])
+            wordSearchQuery.startSearchQuery('AZ', [
+                ['A', 'D', 'D', 'D'], 
+                ['D', 'Z', 'D', 'D']])
         ).toBe('(0, 0), (1, 1)')
     });
 
     test("diagonal DOWN RIGHT search should find first, second, and third letters and store the correct coordinates even if it comes across the first letter multiple times", () => {
         
         expect(
-            wordSearchQuery.startSearchQuery('XYZ', [['A', 'D', 'D', 'D'], ['D', 'X', 'D', 'D'], ['D', 'Z', 'Y', 'D'], ['D', 'Z', 'D', 'Z']])
+            wordSearchQuery.startSearchQuery('XYZ', [
+                ['A', 'D', 'D', 'D'], 
+                ['D', 'X', 'D', 'D'], 
+                ['D', 'Z', 'Y', 'D'], 
+                ['D', 'Z', 'D', 'Z']])
         ).toBe('(1, 1), (2, 2), (3, 3)')
     });
 });
@@ -234,7 +282,9 @@ describe("solution should find words to find on the board DIAGONALLU DOWN AND LE
     test("diagonal DOWN LEFT search should find the first and second letter of a word when downward search is called directly", () => {
         wordSearchQuery.foundWord = '(0, 1)'
 
-        wordSearchQuery.searchDiagonallyDownAndLeft('AZ', [['D','A'],['Z','D']],  0, 1)
+        wordSearchQuery.searchDiagonallyDownAndLeft('AZ', [
+            ['D','A'],
+            ['Z','D']],  0, 1)
 
         expect(
             wordSearchQuery.foundWord
@@ -244,7 +294,9 @@ describe("solution should find words to find on the board DIAGONALLU DOWN AND LE
     test("diagonal DOWN LEFT search should not check for a letter in a row that does not exist", () => {
         wordSearchQuery.foundWord = 'testing non-existant rows'
 
-        wordSearchQuery.searchDiagonallyDownAndLeft('AZ', [['D','D'],['A','Z']],  1, 0)
+        wordSearchQuery.searchDiagonallyDownAndLeft('AZ', [
+            ['D','D'],
+            ['A','Z']],  1, 0)
 
         expect(
             wordSearchQuery.foundWord
@@ -254,14 +306,20 @@ describe("solution should find words to find on the board DIAGONALLU DOWN AND LE
     test("diagonal DOWN LEFT search should find the first and second letter of a word when startSearchQuery is called", () => {
           
         expect(
-            wordSearchQuery.startSearchQuery('AZ', [['D', 'D', 'A', 'D'], ['D', 'Z', 'D', 'D']])
+            wordSearchQuery.startSearchQuery('AZ', [
+                ['D', 'D', 'A', 'D'], 
+                ['D', 'Z', 'D', 'D']])
         ).toBe('(0, 2), (1, 1)')
     });
 
     test("diagonal DOWN LEFT search should find first, second, and third letters and store the correct coordinates even if it comes across the first letter multiple times", () => {
           
         expect(
-            wordSearchQuery.startSearchQuery('XYZ', [['A', 'D', 'D', 'D'], ['D', 'D', 'D', 'X'], ['D', 'Z', 'Y', 'D'], ['D', 'Z', 'D', 'Z']])
+            wordSearchQuery.startSearchQuery('XYZ', [
+                ['A', 'D', 'D', 'D'], 
+                ['D', 'D', 'D', 'X'], 
+                ['D', 'Z', 'Y', 'D'], 
+                ['D', 'Z', 'D', 'Z']])
         ).toBe('(1, 3), (2, 2), (3, 1)')
     }); 
 });
@@ -282,7 +340,9 @@ describe("solution should find words to find on the board DIAGONALLU DOWN AND LE
     test("diagonal UP AND RIGHT search should find the first and second letter of a word when downward search is called directly", () => {
         wordSearchQuery.foundWord = '(1, 0)'
 
-        wordSearchQuery.searchDiagonallyUpAndRight('AZ', [['D','Z'],['A','D']],  1, 0)
+        wordSearchQuery.searchDiagonallyUpAndRight('AZ', [
+            ['D','Z'],
+            ['A','D']],  1, 0)
 
         expect(
             wordSearchQuery.foundWord
@@ -292,12 +352,34 @@ describe("solution should find words to find on the board DIAGONALLU DOWN AND LE
     test("diagonal UP and RIGHT search should not check for a letter in a row that does not exist", () => {
         wordSearchQuery.foundWord = 'testing non-existant rows'
 
-        wordSearchQuery.searchDiagonallyUpAndRight('AZ', [['A','Z'],['D','D']],  0, 0)
+        wordSearchQuery.searchDiagonallyUpAndRight('AZ', [
+            ['A','Z'],
+            ['D','D']],  0, 0)
 
         expect(
             wordSearchQuery.foundWord
         ).toBe('testing non-existant rows')
     });
+
+    test("diagonal UP and RIGHT search should find the first and second letter of a word when startSearchQuery is called", () => {
+            
+        expect(
+            wordSearchQuery.startSearchQuery('AZ', [
+                ['D', 'D', 'Z', 'D'], 
+                ['D', 'A', 'D', 'D']])
+        ).toBe('(0, 2), (1, 1)')
+    });
+
+    test("diagonal UP and RIGHT search should find first, second, and third letters and store the correct coordinates even if it comes across the first letter multiple times", () => {
+            
+        expect(
+            wordSearchQuery.startSearchQuery('XYZ', [
+                ['A', 'D', 'D', 'D'], 
+                ['D', 'D', 'D', 'Z'], 
+                ['D', 'Z', 'Y', 'D'], 
+                ['D', 'X', 'D', 'Z']])
+        ).toBe('(1, 3), (2, 2), (3, 1)')
+    }); 
 
     
 });
