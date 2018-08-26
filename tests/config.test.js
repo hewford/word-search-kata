a458e343584fc5e9d8659e23dabc56fa6b0ed16f
Matthew Keller <matthewkeller@Matthews-MacBook-Pro.local>
2018-08-26 13:06:46 +0400

    tested both configuration variables to not begin their string with a space

--- tests/config.test.js
+++ tests/config.test.js
@@ -2,17 +2,28 @@ const configVariables = require('../config');
 
 describe("configuration variables should have correct format", () => {
   
-    test("the board represented as a string should not have a space at the end of the string", () => {
+    test("the board represented as a string should not have a space at the END of the string", () => {
       let board = configVariables.BOARD_AS_SINGLE_STRING
 
       expect(board[board.length-1]).not.toBe(' ')
     });
 
+    test("the board represented as a string should not have a space at the BEGINNING of the string", () => {
+        let board = configVariables.BOARD_AS_SINGLE_STRING
+  
+        expect(board[0]).not.toBe(' ')
+      });
 
-    test("the string of words to search for should not have a space at the end of the string", () => {
+    test("the string of words to search for should not have a space at the END of the string", () => {
         let words = configVariables.WORDS_TO_SEARCH_FOR
   
         expect(words[words.length-1]).not.toBe(' ')
+      });
+
+      test("the string of words to search for should not have a space at the BEGINNING of the string", () => {
+        let words = configVariables.WORDS_TO_SEARCH_FOR
+  
+        expect(words[0]).not.toBe(' ')
       });
 
   });
