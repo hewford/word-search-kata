93301f7f57bce62cebe60a0953fbb56bf91d1005
Matthew Keller <matthewkeller@Matthews-MacBook-Pro.local>
2018-08-26 13:44:21 +0400

    tested config file for longest word shorter than rows

--- tests/config.test.js
+++ tests/config.test.js
@@ -1,9 +1,11 @@
 const configVariables = require('../config');
 
 describe("configuration variables should have correct format", () => {
+
+    const board = configVariables.BOARD_AS_SINGLE_STRING
+    const words = configVariables.WORDS_TO_SEARCH_FOR
   
     test("the board represented as a string should not have a space at the END of the string", () => {
-      let board = configVariables.BOARD_AS_SINGLE_STRING
 
       jest.setTimeout(1000);
 
@@ -22,15 +24,26 @@ describe("configuration variables should have correct format", () => {
       });
 
     test("the string of words to search for should not have a space at the END of the string", () => {
-        let words = configVariables.WORDS_TO_SEARCH_FOR
-  
         expect(words[words.length-1]).not.toBe(' ')
       });
 
-      test("the string of words to search for should not have a space at the BEGINNING of the string", () => {
-        let words = configVariables.WORDS_TO_SEARCH_FOR
-  
+    test("the string of words to search for should not have a space at the BEGINNING of the string", () => {
         expect(words[0]).not.toBe(' ')
+    });
+
+    test("the square root of board length should have a remainder of 0 to represent a square", () => {
+        expect(Math.sqrt(board.split(' ').length) % 1 === 0).toBe(true)
       });
+
+    test("the square root of board length should have a remainder of 0 to represent a square", () => {
+    
+    const rowLength = Math.sqrt(board.split(' ').length)
+
+    const longestWord = words.split(' ').sort(function(a, b) {
+        return b.length - a.length;
+      })[0];
+    
+    expect(rowLength).toBeGreaterThan(longestWord.length)
+    });
 
   });
