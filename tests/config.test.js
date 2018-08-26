ec21e30935866b00a3eb43d66c6f0a2dcb4ebdb8
Matthew Keller <matthewkeller@Matthews-MacBook-Pro.local>
2018-08-26 13:30:35 +0400

    added config.test run script, and added a thrown error is BOARD_AS_SINGLE_STRING has a space at the end

--- tests/config.test.js
+++ tests/config.test.js
@@ -5,6 +5,13 @@ describe("configuration variables should have correct format", () => {
     test("the board represented as a string should not have a space at the END of the string", () => {
       let board = configVariables.BOARD_AS_SINGLE_STRING
 
+      jest.setTimeout(1000);
+
+      // This is the most common user error when setting up the configuration. So I wanted it throw a big error. 
+      if ( board[board.length-1] == ' ') {
+        throw console.error('=====================================================\nTHERE IS A SPACE AT THE END OF BOARD_AS_SINGLE_STRING\n=====================================================');
+      }
+        
       expect(board[board.length-1]).not.toBe(' ')
     });
 
